9b31bd66d44bc2317c613a88f7d4445b0ee3572e
stuartm2006 <169000455+mvngo1@users.noreply.github.com>
2025-07-10 13:48:39 -0400

    Added alt titles and file names for the photos of part 2

--- assignment4/part 2/gallery-start/main.js
+++ assignment4/part 2/gallery-start/main.js
@@ -3,16 +3,22 @@ const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
-
 /* Declaring the array of image filenames */
-
+const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg","pic5.jpg"];
 /* Declaring the alternative text for each image file */
-
+const altTitles = {
+[fileNames[0]]:"Picture of close up eye",
+[fileNames[1]]:"Image of rock face",
+[fileNames[2]]:"Picture Of Blue & White Flowers",
+[fileNames[3]]:"Old Heiroglyphics on wall",
+[fileNames[4]]:"Picture of butterfly on a leaf",
+};
 /* Looping through images */
-
+for (const file in fileNames)
+{
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', `images/${file}`);
+newImage.setAttribute('alt', [altTitles[file]]);
 thumbBar.appendChild(newImage);
-
+};
 /* Wiring up the Darken/Lighten button */
