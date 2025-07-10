4f9d4a2aaafef386e76de50d82fb029fbabddd2e
stuartm2006 <169000455+mvngo1@users.noreply.github.com>
2025-07-10 13:18:30 -0400

    setup Main.JS based on the tutorial

---
+++ assignment4/part 1/main.js
@@ -0,0 +1,44 @@
+/*
+Filename: Main.js
+Name: Stuart Moss
+Date Last Modified: July 10th, 2025
+Description: The JavaScript Portion of part 1 of assignment 4, silly story generator.
+*/
+
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk" ,"turned into a slug and crawled away"];
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result);
+
+function result() {
+  
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
